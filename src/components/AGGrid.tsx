e8b5f5d3c94d0e6cca3a6b47930360eb7844e1a5
John Dunn <johnwdunn20@gmail.com>
2024-03-19 14:23:55 -0700

    ag grid working

--- src/components/AGGrid.tsx
+++ src/components/AGGrid.tsx
@@ -1,37 +1,48 @@
-import React, { useState } from 'react'
-import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
-import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
-import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import { ColDef } from 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+import { AgGridReact } from 'ag-grid-react';
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
 
-const GridExample = () => {
-  // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ]);
-  
-  // Column Definitions: Defines the columns to be displayed.
-  const [colDefs, setColDefs] = useState([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
-  ]);
-
-  return (
-    // wrapping container with theme & size
-    <div
-     className="ag-theme-quartz" // applying the grid theme
-     style={{ height: 500 }} // the grid will fill the size of the parent container
-    >
-      <AgGridReact
-          rowData={rowData}
-          columnDefs={colDefs}
-      />
-    </div>
-  )
-
+// Row Data Interface
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
 }
 
+// Create new GridExample component
+const GridExample = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState<IRow[]>([
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+    { make: 'Mercedes', model: 'EQA', price: 48890, electric: true },
+    { make: 'Fiat', model: '500', price: 15774, electric: false },
+    { make: 'Nissan', model: 'Juke', price: 20675, electric: false },
+  ]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ]);
+
+  // Container: Defines the grid's theme & dimensions.
+  return (
+    <div
+      className={
+        "ag-theme-quartz"
+      }
+      style={{ width: '100%', height: '100%' }}
+    >
+      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    </div>
+  );
+};
 export default GridExample;
