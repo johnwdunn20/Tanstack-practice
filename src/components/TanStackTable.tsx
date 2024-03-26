94d6b1c470eae78b6706de6717ed4f97680903f0
John Dunn <johnwdunn20@gmail.com>
2024-03-25 20:30:42 -0700

    updated tanstack table

--- src/components/TanStackTable.tsx
+++ src/components/TanStackTable.tsx
@@ -1,14 +1,14 @@
 import React, { useMemo, useState } from 'react'
-import { ColumnDef, useReactTable } from '@tanstack/react-table'
+import { useReactTable } from '@tanstack/react-table'
 
 //TData
 type User = {
   firstName: string
   lastName: string
-  age: number
-  visits: number
-  progress: number
-  status: string
+  // age: number
+  // visits: number
+  // progress: number
+  // status: string
 }
 
 const TanStackTable = () => {
@@ -16,26 +16,38 @@ const TanStackTable = () => {
       {
         "firstName": "Tanner",
         "lastName": "Linsley",
-        "age": 33,
-        "visits": 100,
-        "progress": 50,
-        "status": "Married"
+        // "age": 33,
+        // "visits": 100,
+        // "progress": 50,
+        // "status": "Married"
       },
       {
         "firstName": "Kevin",
         "lastName": "Vandy",
-        "age": 27,
-        "visits": 200,
-        "progress": 100,
-        "status": "Single"
+        // "age": 27,
+        // "visits": 200,
+        // "progress": 100,
+        // "status": "Single"
       }
   ])
 
-  const columns: ColumnDef<User>[] = []
-  // const table = useReactTable({
-  //   columns,
-  //   data
-  // })
+  const columns = [
+    {
+      accessorKey: 'firstName', // accessorKey: 'firstName' is the same as accessorFn: (row: User) => row.firstName??
+      id: 'firstName',
+      header: 'First Name',
+    },
+    {
+      accessorKey: 'lastName',
+      id: 'lastName',
+      header: 'Last Name',
+    },
+  ];
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel => getCoreRowModel(),
+  })
 
   return (
     <div>TanStackTable</div>
