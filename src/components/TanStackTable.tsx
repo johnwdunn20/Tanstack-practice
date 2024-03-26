a02155308993f10265a60fa8d387d99d858b2314
John Dunn <johnwdunn20@gmail.com>
2024-03-26 12:00:30 -0700

    updated tanstack table

--- src/components/TanStackTable.tsx
+++ src/components/TanStackTable.tsx
@@ -1,59 +1,89 @@
-import React, { useMemo, useState } from 'react'
-import { useReactTable, getCoreRowModel } from '@tanstack/react-table'
+import React, { useMemo, useState } from "react";
+import {
+  useReactTable,
+  getCoreRowModel,
+  ColumnDef,
+} from "@tanstack/react-table";
 
 //TData
 type User = {
-  firstName: string
-  lastName: string
+  firstName: string;
+  lastName: string;
   // age: number
   // visits: number
   // progress: number
   // status: string
-}
+};
 
 const TanStackTable = () => {
   const [data, setData] = useState<User[]>([
-      {
-        "firstName": "Tanner",
-        "lastName": "Linsley",
-        // "age": 33,
-        // "visits": 100,
-        // "progress": 50,
-        // "status": "Married"
-      },
-      {
-        "firstName": "Kevin",
-        "lastName": "Vandy",
-        // "age": 27,
-        // "visits": 200,
-        // "progress": 100,
-        // "status": "Single"
-      }
-  ])
+    {
+      firstName: "Tanner",
+      lastName: "Linsley",
+      // "age": 33,
+      // "visits": 100,
+      // "progress": 50,
+      // "status": "Married"
+    },
+    {
+      firstName: "Kevin",
+      lastName: "Vandy",
+      // "age": 27,
+      // "visits": 200,
+      // "progress": 100,
+      // "status": "Single"
+    },
+  ]);
 
   // useMemo is a React hook that memorizes the output of a function and reuses it when the inputs change.
   // This prevents unnecessary recalculations of the column array on every render
-  const columns = useMemo(() => [
-    {
-      accessorKey: 'firstName', // accessorKey: 'firstName' is the same as accessorFn: (row: User) => row.firstName??
-      id: 'firstName',
-      header: 'First Name',
-    },
-    {
-      accessorKey: 'lastName',
-      id: 'lastName',
-      header: 'Last Name',
-    },
-  ], []);
+  const columns: ColumnDef<User>[] = useMemo(
+    () => [
+      {
+        accessorKey: "firstName", // accessorKey: 'firstName' is the same as accessorFn: (row: User) => row.firstName??
+        id: "firstName",
+        header: "First Name",
+      },
+      {
+        accessorKey: "lastName",
+        id: "lastName",
+        header: "Last Name",
+      },
+    ],
+    []
+  );
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-  })
+  });
 
   return (
-    <div>TanStackTable</div>
-  )
-}
+    <div>
+      {/* <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.column.columnDef.header}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{cell.getValue()}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table> */}
+    </div>
+  );
+};
 
-export default TanStackTable
+export default TanStackTable;
